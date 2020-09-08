0cacb2fe9f31f9e20ca0b1b6695965a16e12ba02
markusanton <markusa070@gmail.com>
2020-09-08 11:51:32 +0300

    Add project files.

---
+++ StringInter/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace StringInter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your first name:");
+            var firstName = Console.ReadLine();
+            Console.WriteLine("Enter your last name:");
+            var lastName = Console.ReadLine();
+            Console.WriteLine($"Hello, {firstName} {lastName}");
+        }
+    }
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName;
+            string lastName;
+            string userInputyearOfBirth;
+            
+            Console.WriteLine("What's your first name?");
+            firstName = Console.ReadLine();
+
+            Console.WriteLine("What's your last name?");
+            lastName = Console.ReadLine();
+
+            Console.WriteLine("When were you born?");
+            userInputyearOfBirth = Console.ReadLine();
+            int yearOfBirthint;
+            yearOfBirthint = Int32.Parse(userInputyearOfBirth);
+            int yearofBirthFinal = 2020 - yearOfBirthint;
+
+            Console.WriteLine($"Hello, {firstName} {lastName}. You are {yearofBirthFinal} years old.");
+        
+        }
+    }
+}
